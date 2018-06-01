ad06342d6b26d269ecb519590ccb23d600e0d87e
Dmytro V <staff1489@gmail.com>
2018-06-01 12:09:07 +0300

    #1134: removing calls to Member::Levels.get

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180529125011) do
+ActiveRecord::Schema.define(version: 20180530122201) do
 
   create_table "accounts", force: :cascade do |t|
     t.integer  "member_id",   limit: 4,                                          null: false
@@ -118,7 +118,7 @@ ActiveRecord::Schema.define(version: 20180529125011) do
     t.boolean  "api_disabled",             default: false, null: false
     t.datetime "created_at",                               null: false
     t.datetime "updated_at",                               null: false
-    t.string   "level",        limit: 255, default: "",    null: false
+    t.integer  "level",        limit: 4,   default: 0,     null: false
   end
 
   add_index "members", ["disabled"], name: "index_members_on_disabled", using: :btree
